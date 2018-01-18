156e72385b4ba9e77313004e43f5e6a9768fbd8a
1720966051@qq.com <1720966051@qq.com>
2018-01-18 08:20:50 +0800

    qq-music

---
+++ QQ Music/js/slider.js
@@ -0,0 +1,37 @@
+export class Slider {
+    constructor(options) {
+        this.$el = options.el;
+        this.slides = options.slides;
+        this.interval = options.interval || 3000;
+        this.render();
+        this.index = 0;
+        this.start();
+    }
+
+    render() {
+        this.$el.innerHTML = '<div class="qq-slider-wrap"></div>';
+        this.$wrap = this.$el.firstElementChild;
+        this.$wrap.style.width = `${this.slides.length * 100}%`;
+        this.$wrap.innerHTML = this.slides.map(slide =>
+            `<div class="qq-slider-item">
+                <a href="${slide.link}">
+                    <img src="${slide.image}" alt="图片">
+                </a>
+            </div>`
+        ).join('')
+    }
+
+    start() {
+        setInterval(this.next.bind(this), this.interval)
+    }
+
+    next() {
+        this.index += 1
+        if (this.index === this.slides.length) {
+          this.$wrap.style.transform = `translate(0)`
+          this.index = 0
+          return
+        }
+        this.$wrap.style.transform = `translate(-${this.index * 100 / this.slides.length}%)`
+    }
+}
---
+++ QQ Music/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './js/app.js',
+    output: {
+        filename: 'dist/app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+}
