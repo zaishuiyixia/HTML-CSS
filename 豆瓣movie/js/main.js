fb55153c102399bd7cfc0c7d539bb588a75f787d
Xu GuoJun <1720966051@qq.com>
2018-02-02 18:38:19 +0800

    Update main.js

--- 豆瓣movie/js/main.js
+++ 豆瓣movie/js/main.js
@@ -1,100 +1,3 @@
-// $('footer>div').click(function() {
-//     var index = $(this).index();
-//     $('section').hide().eq(index).fadeIn();
-//     $(this).addClass('active').siblings().removeClass('active');
-// });
-
-// var index = 0;
-// var isLoading = false;
-// start();
-
-// function start() {
-//     if (isLoading) return;
-//     isLoading = true;
-//     $('.loading').show();
-//     $.ajax({
-//         url: 'http://api.douban.com/v2/movie/top250',
-//         type: 'GET',
-//         data: {
-//             start: index,
-//             count: 20
-//         },
-//         dataType: 'jsonp'
-//     }).done(function(ret) {
-//         console.log(ret);
-//         setData(ret);
-//         index += 20;
-//     }).fail(function() {
-//         console.log('error ...')
-//     }).always(function() {
-//         isLoading = false;
-//         $('.loading').hide();
-//     })
-// }
-// var clock
-// $('main').scroll(function() {
-//     if (clock) {
-//         clearTimeout(clock);
-//     }
-//     clock = setTimeout(function() {
-//         if ($('section').eq(0).height() - 10 <= $('main').scrollTop() + $('main').height()) {
-//             start()
-//         }
-//     }, 300)
-// })
-
-// function setData(data) {
-//     data.subjects.forEach(function(movie) {
-//         var template = `<div class="item">
-// 				<a href="#">
-// 					<div class="cover">
-// 						<img src="" alt="">
-// 					</div>
-// 					<div class="detail">
-// 						<h2></h2>
-// 						<div class="extra">
-// 							<span class="score">分 / </span>
-// 							<span class="collect"></span>收藏
-// 						</div>
-// 						<div class="extra">
-// 							<span class="year"></span>
-// 							<span class="type"></span>
-// 						</div>
-// 						<div class="extra">导演：<span class="director"></span></div>
-// 						<div class="extra">主演：<span class="actor"></span></div>
-// 					</div>
-// 				</a>
-// 			</div>`
-
-//         var $node = $(template);
-//         $node.find('.cover img').attr('src', movie.images.medium);
-//         $node.find('.detail h2').text(movie.title);
-//         $node.find('.score').text(movie.rating.average);
-//         $node.find('.collect').text(movie.collect_count);
-//         $node.find('.year').text(movie.year);
-//         $node.find('.type').text(movie.genres.join(' / '));
-//         $node.find('a').attr("href", movie.alt);
-//         $node.find('.director').text(function() {
-//             var directorsArr = [];
-//             movie.directors.forEach(function(item) {
-//                 directorsArr.push(item.name)
-//             })
-//             return directorsArr.join('、');
-//         })
-//         $node.find('.actor').text(function() {
-//             var actorArr = [];
-//             movie.casts.forEach(function(item) {
-//                 actorArr.push(item.name)
-//             });
-//             return actorArr.join('、')
-//         })
-//         $('#top250').eq(0).append($node)
-//     });
-// }
-
-
-
-
 var top250 = {
     init: function() {
         this.$element = $('#top250');
@@ -130,7 +33,7 @@ var top250 = {
         _this.isLoading = true;
         _this.$element.find('.loading').show();
         $.ajax({
-            url: 'http://api.douban.com/v2/movie/top250',
+            url: 'https://api.douban.com/v2/movie/top250',
             type: 'GET',
             data: {
                 start: _this.index || 0,
@@ -221,7 +124,7 @@ var usBox = {
 		_this.isLoading = true
 		_this.$element.find('.loading').show()
 		$.ajax({
-			url: 'http://api.douban.com/v2/movie/us_box',
+			url: 'https://api.douban.com/v2/movie/us_box',
 			dataType: 'jsonp'
 		}).done(function(ret){
 			callback&&callback(ret)
@@ -303,7 +206,7 @@ var search = {
 		var _this = this
 		_this.$element.find('.loading').show()
 		$.ajax({
-			url: 'http://api.douban.com/v2/movie/search',
+			url: 'https://api.douban.com/v2/movie/search',
 			data: {
 			  q: _this.keyword
 			},
